f8433f843880de7bbfaff33969ce3781c9ab03d1
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-01 15:52:41 +0530

    async engine created

--- app/db/database.py
+++ app/db/database.py
@@ -1,3 +1,13 @@
 # Create SQLALchemy engine -> to connect to Postgres SQL
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from app.core.config import settings
+from app.db.base import Base
+
+engine = create_async_engine(
+    settings.database_url,
+    echo=settings.debug,
+    pool_pre_ping=True, #checks if connection is still valid if not then create a new one
+)
+
+
+
