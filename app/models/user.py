1722d11008dea0800c66c3a9fdaa4a7be44563cc
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-01 22:07:18 +0530

    minor changes

--- app/models/user.py
+++ app/models/user.py
@@ -13,12 +13,10 @@ from app.models.mixins import TimestampMixin, UUIDMixin
 
 class User(Base, TimestampMixin,UUIDMixin):
     __tablename__ = "users"
-    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
     email: Mapped[str] = mapped_column(String(255),unique=True, nullable=False, index=True)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     phone_number: Mapped[str | None] = mapped_column(String(20), nullable=True)
     full_name: Mapped[str] = mapped_column(String(100), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean,default=True, nullable=False)
     is_verified: Mapped[bool] = mapped_column(Boolean,default=False, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(server_default=func.now(),nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now(), nullable=False)
+  
