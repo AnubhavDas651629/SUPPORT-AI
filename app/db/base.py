0f4822fdd3a7d947af15829b1f7ce375262bdb7a
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-01 16:54:37 +0530

    User class done

--- app/db/base.py
+++ app/db/base.py
@@ -1,1 +1,5 @@
 #Declarative base that our models inherit from
+from sqlalchemy.orm import DeclarativeBase
+
+class Base(DeclarativeBase):
+    pass
---
+++ app/models/user.py
@@ -0,0 +1,20 @@
+from time import timezone
+from tokenize import String
+from turtle import update
+from uuid import UUID, uuid4
+from sqlalchemy import Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from app.db.base import Base
+from datetime import datetime, UTC
+from sqlalchemy import Boolean, func
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[UUID] = mapped_column(default=uuid4, primary_key=True)
+    email: Mapped[str] = mapped_column(String(255),unique=True, nullable=False, index=True)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean,default=True, nullable=False)
+    is_verified: Mapped[bool] = mapped_column(Boolean,default=False, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now(),nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(server_default=func.now(), onupdate=func.now(), nullable=False)
