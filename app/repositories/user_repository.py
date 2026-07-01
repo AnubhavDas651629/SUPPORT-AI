f9b496a749a5cfa77c7aa307eda95479fa56c60d
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-01 22:47:19 +0530

    email repo

--- app/repositories/user_repository.py
+++ app/repositories/user_repository.py
@@ -1,8 +1,17 @@
 from uuid import UUID
-from sqlalchemy import select
+from sqlalchemy import select, String
 
 from app.models.user import User
 from app.repositories.base import BaseRepository
 
 class UserRepository(BaseRepository):
-    pass
+    async def get_by_id(self, user_id: UUID) -> User | None:
+        query = select(User).where(User.id == user_id)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_by_email(self, email: str) -> User | None:
+        query = select(User).where(User.email == email)
+        result = await self.session.execute(query)
+        return result.scalar_one_or_none()
+        
