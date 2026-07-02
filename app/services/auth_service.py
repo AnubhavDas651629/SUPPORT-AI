dd66e2944df3df146af6244276a253afb86ba8a3
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-02 17:23:55 +0530

    register method done

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.repositories.user_repository import UserRepository
+from app.core.security import hash_password
 
 class AuthService:
     def __init__(self, session: AsyncSession):
@@ -8,3 +9,19 @@ class AuthService:
 
     async def register(self, *, email:str, password:str, full_name:str):
         existing_user = await self.user_repository.get_by_email(email)
+        if existing_user:
+            raise ValueError("User with this email already exists")
+
+        hashed_password = hash_password(password)
+
+        user = await self.user_repository.create(
+            email=email,
+            hashed_password=hashed_password,
+            full_name=full_name,
+        )
+
+        await self.session.commit()
+
+        return user
+
+
