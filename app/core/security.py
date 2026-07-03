2f9b615d7ba123f57e25620af562deb33118b489
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-03 10:32:14 +0530

    login feature added

--- app/core/security.py
+++ app/core/security.py
@@ -16,3 +16,22 @@ def create_access_token(user_id:str) -> str:
     expire = datetime.now(UTC) + timedelta(
         minutes=settings.access_token_expire_minutes
     )
+
+    payload = {
+        "sub": user_id,
+        "exp": expire
+    }
+
+    return jwt.encode(
+        payload,
+        settings.jwt_secret,
+        algorithm=settings.jwt_algorithm,
+
+    )
+
+def decode_access_token(token: str) -> dict:
+    return jwt.decode(
+        token,
+        settings.jwt_secret,
+        algorithms=[settings.jwt_algorithm],
+    )
--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.repositories.user_repository import UserRepository
-from app.core.security import hash_password
+from app.core.security import create_access_token, hash_password, verify_password
+from app.core.exception_handlers import UserAlreadyExistsException
+from app.schemas.auth import TokenResponse
 
 class AuthService:
     def __init__(self, session: AsyncSession):
@@ -10,7 +12,7 @@ class AuthService:
     async def register(self, *, email:str, password:str, full_name:str):
         existing_user = await self.user_repository.get_by_email(email)
         if existing_user:
-            raise ValueError("User with this email already exists")
+            raise UserAlreadyExistsException()
 
         hashed_password = hash_password(password)
 
@@ -24,4 +26,23 @@ class AuthService:
 
         return user
 
+    async def login(self, *, email:str, password: str):
+        user = await self.user_repository.get_by_email(email)
+
+        if not user:
+            raise InvalidCredentialException()
+
+        if not verify_password(
+            password, 
+            user.hashed_password
+        ): 
+            raise InvalidCredentialException()
+
+        token = create_access_token(
+            str(user.id),
+        )
+
+        return TokenResponse(
+            access_token=token,
+        )
 
