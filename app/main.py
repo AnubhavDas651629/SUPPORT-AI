2715c52448c1592550cccf9511d0fef13e17149f
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-03 11:34:49 +0530

    added users route

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from sqlalchemy import text
 from app.db.dependencies import get_db
 from app.api.v1.auth import router as auth_router
+from app.api.v1.users import router as users_router
 from app.core.exception_handlers import (register_exception_handlers)
 
 
@@ -16,6 +17,11 @@ register_exception_handlers(app)
 
 app.include_router(
     auth_router,
+    prefix="/api/v1",
+)
+
+app.include_router(
+    users_router,
     prefix="/api/v1",
 )
 
