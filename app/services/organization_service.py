74bd908fc4a01c349f3ef1bb25947b86c4139078
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-04 17:24:44 +0530

    invited user auth check

--- app/services/organization_service.py
+++ app/services/organization_service.py
@@ -6,20 +6,16 @@ from app.exceptions.organization import OrganizationNotFoundException
 from app.models import Organization, organization
 from app.models.organization_member import OrganizationRole
 from app.models.user import User
-from app.repositories import Organization_member_repository
+from app.repositories.user_repository import UserRepository
 from app.repositories.organization_member_repository import OrganizationMemberRepository
 from app.repositories.organization_repository import OrganizationRepository
 
 class OrganizationService:
     def __init__(self, session: AsyncSession):
         self.session = session
-        self.organization_repository = OrganizationRepository(
-            session
-        )      
-        self.membership_repository = (
-            OrganizationMemberRepository(session)
-        )  
-
+        self.organization_repository = OrganizationRepository(session)      
+        self.membership_repository = OrganizationMemberRepository(session)
+        self.user_repository = UserRepository(session)
     async def create(self, *, current_user: User, name: str):
         slug = slugify(name)
 
@@ -56,8 +52,43 @@ class OrganizationService:
     async def invite_member(self, *, organization_id: UUID, current_user: User, email:str, role: OrganizationRole):
         organization = await self.organization_repository.get_by_id_for_user(
             organization_id=organization_id,
-            user_id=current_user.user.id
+            user_id=current_user.id
         )
         if organization is None:
             raise OrganizationNotFoundException()
-            
+
+        # if this function returns org this means we have identified the org and the current user is a part of the org, now in order to invite, next step is to check the role of the current user, that decides he they can invite a person
+        membership = await self.membership_repository.get_membership(
+            organization_id=organization_id,
+            user_id=current_user.id,
+        )
+        if (membership is None or membership.role != OrganizationRole.OWNER):
+            raise PermissionDeniedException()
+
+        #if till here this means the inviter is the owner
+        #now we check is the invited user has registered or not
+        invited_user = await self.user_repository.get_by_email(
+            email=email
+        )
+        if invited_user is None:
+            raise UserNotFoundException()
+
+        #Now we are checking if the invited member is already a part of the org or not as we donnot want duplicates
+        existing_membership = await self.membership_repository.get_membership(
+            organization_id=organization_id,
+            user_id=invited_user.id
+        )
+        if existing_membership is not None:
+            raise AlreadyOrganizationMemberException()
+
+        created_membership = await self.membership_repository.create(
+            organization_id=organization_id,
+            user_id=invited_user.id,
+            role=role,
+        )
+        await self.session.commit()
+        return created_membership
+
+
+
+
