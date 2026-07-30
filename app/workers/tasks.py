e07865191325c42290ad5ffe21b2778e76216bde
Anubhavdas651 <anubhavdas651@gmail.com>
2026-07-30 11:24:10 +0530

    added celery worker for retry email service

--- app/workers/tasks.py
+++ app/workers/tasks.py
@@ -4,6 +4,7 @@ from app.workers.celery_app import celery_app
 from asyncio import run
 from app.db.session import AsyncSessionLocal
 from app.services.email_service import EmailService
+from app.services.email_service import EmailService
 
 logger = get_task_logger(__name__)
 
@@ -22,3 +23,18 @@ def send_ticket_create_email(ticket_id: str):
                 ticket_id=ticket_id
             )
     run(_send())
+
+@celery_app.task(
+    retry_backoff=True,
+    retry_jitter=True,
+    retry_kwargs={"max_retries": 3}
+)
+def send_otp_email_task(email: str, otp: str):
+    logger.info(f"Excecuting send_otp_email_task for: {email}")
+    async def _send():
+        async with AsyncSessionLocal() as session:
+            service = EmailService(session=session)
+            await service.send_otp_email(email=email, otp=otp)
+    run(_send())
+
+    
